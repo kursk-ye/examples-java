f972f4903127523250fa92f7d6e9208064d0af6f
yelei <kursk.ye@gmail.com>
2020-04-24 02:15:24 +0800

    comment

--- src/main/java/io/github/streamingwithflink/chapter6/kursk/DemoKeyedProcessFunction.java
+++ src/main/java/io/github/streamingwithflink/chapter6/kursk/DemoKeyedProcessFunction.java
@@ -16,13 +16,13 @@ public class DemoKeyedProcessFunction extends KeyedProcessFunction<String, ElecM
         Double prevDayElecValue = lastDayElecValue.value();
         lastDayElecValue.update(value.getDayElecValue());
 
-        Long curTimerTimestamp = currentTimer.value(); // ???
+        Long curTimerTimestamp = currentTimer.value(); // 前一个event的时间戳
         if (prevDayElecValue == 0.0 || value.getDayElecValue() < prevDayElecValue) {
-            ctx.timerService().deleteProcessingTimeTimer(curTimerTimestamp);
-            currentTimer.clear();
-        } else if (value.getDayElecValue() > prevDayElecValue && curTimerTimestamp == 0) {
-            Long timerTs = ctx.timerService().currentProcessingTime() + 1000L;
-            ctx.timerService().registerProcessingTimeTimer(timerTs);
+            ctx.timerService().deleteProcessingTimeTimer(curTimerTimestamp);  // 删除计时器上的注册函数
+            currentTimer.clear();                                             // 当前时间戳清零
+        } else if (value.getDayElecValue() > prevDayElecValue && curTimerTimestamp == 0) {  // 第一个温度升高的时刻
+            Long timerTs = ctx.timerService().currentProcessingTime() + 1000L;              // 如果1秒钟后该函数还是被触发，说明1秒钟内函数没有被清零—，即说明1秒钟内温度没有下降，否则会进入上个条件
+            ctx.timerService().registerProcessingTimeTimer(timerTs);                        // 函数会被从计时器移除
             currentTimer.update(timerTs);
         }
     }
